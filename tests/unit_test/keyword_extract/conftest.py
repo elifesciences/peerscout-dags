8e3d41edaada9ea206c390c33597215fc50d09ba
Daniel Ecer <de-code@users.noreply.github.com>
2019-12-11 16:56:38 +0000

    don't load same spacy model twice for tests

--- tests/unit_test/keyword_extract/conftest.py
+++ tests/unit_test/keyword_extract/conftest.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from typing import Dict
 
 import pytest
 
@@ -24,17 +25,32 @@ def _load_spacy_model(language_model_name: str) -> Language:
     return spacy.load(language_model_name)
 
 
+def _get_or_load_spacy_model(
+        language_model_name: str,
+        spacy_model_cache: Dict[str, Language]) -> Language:
+    language_model = spacy_model_cache.get(language_model_name)
+    if language_model is None:
+        language_model = _load_spacy_model(language_model_name)
+        spacy_model_cache[language_model_name] = language_model
+    return language_model
+
+
+@pytest.fixture(name="spacy_model_cache", scope="session")
+def _spacy_model_cache() -> Dict[str, Language]:
+    return {}
+
+
 @pytest.fixture(name="spacy_language_en", scope="session")
-def _spacy_language_en() -> Language:
-    return _load_spacy_model(os.environ.get(
+def _spacy_language_en(spacy_model_cache: Dict[str, Language]) -> Language:
+    return _get_or_load_spacy_model(os.environ.get(
         EnvVars.SPACY_LANGUAGE_EN_MINIMAL,
         DEFAULT_SPACY_LANGUAGE_MODEL_NAME
-    ))
+    ), spacy_model_cache=spacy_model_cache)
 
 
 @pytest.fixture(name="spacy_language_en_full", scope="session")
-def _spacy_language_en_full() -> Language:
-    return _load_spacy_model(os.environ.get(
+def _spacy_language_en_full(spacy_model_cache: Dict[str, Language]) -> Language:
+    return _get_or_load_spacy_model(os.environ.get(
         EnvVars.SPACY_LANGUAGE_EN_FULL,
         DEFAULT_SPACY_LANGUAGE_MODEL_NAME
-    ))
+    ), spacy_model_cache=spacy_model_cache)
