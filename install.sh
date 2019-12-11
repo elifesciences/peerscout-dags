82d6aa32d203defc5b0eab1a69f93657e26e279e
michael <m.owonibi@elifesciences.org>
2019-12-11 10:55:24 +0000

    #5268
    modify install script for k8s airflow  deployment

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+pip install  --user -r $DIR/requirements.spacy.txt
 pip install  --user -r $DIR/requirements.txt
-
 pip install -e $DIR/ --user --no-dependencies
+
+python -m spacy download en_core_web_lg
+
 cp $DIR/dags $1 -r
-
-
