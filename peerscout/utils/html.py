15524afdf72fc084be031ac248c3a9d8d5886ea9
Daniel Ecer <de-code@users.noreply.github.com>
2020-04-23 09:47:42 +0200

    strip html tags before extracting keywords via spacy (#21)
    * strip html tags before extracting keywords via spacy
    
    * added comment
    
    * renamed fed to _collected_data
    
    * removed call to reset
    
    * renamed to MarkupStrippingHtmlParser
    
    * moved convert_charrefs to init argument
    
    * removed setting strict

---
+++ peerscout/utils/html.py
@@ -0,0 +1,23 @@
+from html.parser import HTMLParser
+
+
+class MarkupStrippingHtmlParser(HTMLParser):
+    def __init__(self, convert_charrefs: bool = True):
+        super().__init__(convert_charrefs=convert_charrefs)
+        self._collected_data = []
+
+    def handle_data(self, data):
+        self._collected_data.append(data)
+
+    def get_data(self):
+        return ''.join(self._collected_data)
+
+    def error(self, message):
+        # override ParserBase.error, which otherwise raises NotImplementedError
+        raise message
+
+
+def strip_tags(text: str) -> str:
+    stripper = MarkupStrippingHtmlParser()
+    stripper.feed(text)
+    return stripper.get_data()
