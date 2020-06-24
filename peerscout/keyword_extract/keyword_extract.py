48a98a8cfede2393abf4947edb8a9125d8ae369f
Daniel Ecer <de-code@users.noreply.github.com>
2020-06-24 10:06:44 +0100

    renamed to iterator

--- peerscout/keyword_extract/keyword_extract.py
+++ peerscout/keyword_extract/keyword_extract.py
@@ -8,7 +8,7 @@ import re
 import logging
 from tempfile import TemporaryDirectory
 from pathlib import Path
-from typing import Iterable, List, Tuple
+from typing import Iterable, Iterator, List, Tuple, T
 import datetime
 from itertools import tee
 from datetime import timezone
@@ -313,12 +313,12 @@ def write_to_jsonl_file(
             write_file.write("\n")
 
 
-def iter_get_batches(iterable, size):
+def iter_get_batches(iterator: Iterator[T], size: int) -> Iterable[T]:
     while True:
         chunk = []
         for _ in range(size):
             try:
-                chunk.append(next(iterable))
+                chunk.append(next(iterator))
             except StopIteration:
                 if chunk:
                     yield chunk
