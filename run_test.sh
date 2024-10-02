c18caad11d12f3bfb95a377636d27a84a4e69a73
Hazal <HazalCiplak@users.noreply.github.com>
2024-10-02 14:42:46 +0100

    Remove Airflow dependency (#996)
    * used python image
    
    * removed airflow from requirements
    
    * removed unnecessary dependencies

--- run_test.sh
+++ run_test.sh
@@ -7,21 +7,21 @@ export PYTHONOPTIMIZE=
 
 
 echo "running flake8"
-flake8 tests/ peerscout/
+python -m flake8 tests/ peerscout/
 
 echo "running pylint"
 PYLINTHOME=/tmp/datahub-dags-pylint \
- pylint tests/ peerscout/ --init-hook="import sys; sys.setrecursionlimit(1500)"
+python -m pylint tests/ peerscout/ --init-hook="import sys; sys.setrecursionlimit(1500)"
 
 echo "running mypy"
-mypy --check-untyped-defs peerscout tests
+python -m mypy --check-untyped-defs peerscout tests
 
 echo "running unit tests"
-pytest tests/unit_test/ -p no:cacheprovider -s --disable-warnings
+python -m pytest tests/unit_test/ -p no:cacheprovider -s --disable-warnings
 
 if [[ $1  &&  $1 == "with-end-to-end" ]]; then
     echo "running end to end tests"
-    pytest tests/end2end_test/ -p no:cacheprovider --log-cli-level=INFO
+    python -m pytest tests/end2end_test/ -p no:cacheprovider --log-cli-level=INFO
 fi
 
 echo "done"
